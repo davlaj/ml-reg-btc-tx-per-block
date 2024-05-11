ccdf6228bc4fa1e9575478fd43918904b18380d2
davlaj <davidlajeunesse1+github@gmail.com>
2024-05-11 20:02:21 -0300

    data-cleaning progress

---
+++ scripts/data-cleaning.R
@@ -0,0 +1,162 @@
+# Install packages if they are not already installed
+if (!require("dplyr")) install.packages("dplyr")
+if (!require("ggplot2")) install.packages("ggplot2")
+if (!require("tidyr")) install.packages("tidyr")
+if (!require("lubridate")) install.packages("lubridate", dependencies=TRUE)
+if (!require("purrr")) install.packages("purrr")
+
+# Load packages
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(lubridate)
+library(purrr)
+
+# Load data 
+data <- read.csv("/Users/github/bitcoin-transactions-per-block/data/raw/block_data.csv")
+
+###################
+# Data Formatting #
+###################
+
+# Convert the 'Date' column to POSIXct date-time objects
+data$Date <- as.POSIXct(data$Date, origin="1970-01-01", tz="UTC")
+# View the updated dataframe to confirm changes
+head(data)
+
+# Adding new columns
+## Hour of the day
+data$HourOfDay <- hour(data$Date)
+## Day of the week
+# Returns numbers where 1 = Sunday, 2 = Monday, ..., 7 = Saturday
+data$DayOfWeek <- wday(data$Date) 
+
+################
+# Missing Data #
+################
+
+# Identify missing data
+summary(data)  # Gives a summary, including NA counts for all columns
+
+# Only 28 blocks with missing AverageFee.  
+# These are blocks with only 1 tx, the coinbase tx which doesn't have any fee
+missing_averageFee_rows <- data[is.na(data$AverageFee), ]
+print(missing_averageFee_rows) 
+
+# Decide on a strategy
+## Imputation -> Replace by 0
+data <- data %>%
+  mutate(across(everything(), ~replace(., is.na(.), 0)))
+
+## Deletion
+# data <- na.omit(data)  # Removes all rows with any NA values
+
+############
+# Outliers #
+############
+
+colnames(data)
+
+# Detection
+# Reshape the data and create separate boxplots for all numeric columns
+data %>%
+  select(where(is.numeric)) %>%  # Select only numeric columns
+  pivot_longer(cols = everything(), names_to = "Variable", values_to = "Value") %>%  # Reshape data
+  ggplot(aes(x = "", y = Value)) +  # Setup axes
+  geom_boxplot() +  # Create boxplots
+  facet_wrap(~ Variable, scales = "free_y") +  # Separate plot for each variable with free y scale
+  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) +  # Remove x-axis elements
+  labs(y = "Values", title = "Separate Boxplot for Each Numeric Variable")  # Label the y-axis and provide a title
+
+# # Treatment: Trimming (removing)
+# quantiles <- quantile(data$numeric_column, probs=c(.25, .75), na.rm = TRUE)
+# iqr <- IQR(data$numeric_column, na.rm = TRUE)
+# data <- data %>%
+#   filter(numeric_column >= (quantiles[1] - 1.5 * iqr) & numeric_column <= (quantiles[2] + 1.5 * iqr))
+
+##########################################
+# Correcting Inconsistencies and Scaling #
+##########################################
+
+# Standardization (uniformity/consitency in values)
+# none
+
+# Standardization (Z-score scaling) -> Apply to continuous numeric non-categorical data
+numeric_cols_to_scale <- names(data)[sapply(data, is.numeric) & !names(data) %in% c("BlockNumber", "HourOfDay", "DayOfWeek")]
+data_scaled <- data %>%
+  mutate(across(all_of(numeric_cols_to_scale), scale))
+
+# Boxplot after scaling
+data_scaled %>%
+  select(where(function(x) is.numeric(x)), -c(BlockNumber, HourOfDay, DayOfWeek)) %>%  # Select only numeric columns
+  pivot_longer(cols = everything(), names_to = "Variable", values_to = "Value") %>%  # Reshape data
+  ggplot(aes(x = "", y = Value)) +  # Setup axes
+  geom_boxplot() +  # Create boxplots
+  facet_wrap(~ Variable, scales = "free_y") +  # Separate plot for each variable with free y scale
+  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) +  # Remove x-axis elements
+  labs(y = "Values", title = "Separate Boxplot for Each Non-Categorical Numeric Variables after Scaling")  # Label the y-axis and provide a title
+
+summary(data_scaled)
+
+# Encoding categorical variable
+## One-hot encoding for nominal data
+# Convert HourOfDay and DayOfWeek to factors to ensure model.matrix recognizes them as categorical variables
+data_scaled$HourOfDay <- factor(data_scaled$HourOfDay)
+data_scaled$DayOfWeek <- factor(data_scaled$DayOfWeek)
+# model.matrix will by default drop the first dummy variable of each category if the index start at 1
+# DayOfWeek starts at 1, but HourOfDay start at 0, so we need to drop it to avoid the Dummy Variable Trap
+onehot_encoded <- model.matrix(~ HourOfDay + DayOfWeek - 1, data_scaled) 
+data_encoded_onehot <- cbind(data_scaled, onehot_encoded)
+data_encoded_onehot <- data_encoded_onehot %>% select(-HourOfDay0)  # Remove the HourOfDay0 column
+# Check the structure to confirm changes
+str(data_encoded_onehot)
+# Convert back DayOfWeek and HourOfDay to numeric format 
+data_encoded_onehot$DayOfWeek <- as.numeric(as.character(data_encoded_onehot$DayOfWeek))
+data_encoded_onehot$HourOfDay <- as.numeric(as.character(data_encoded_onehot$HourOfDay))
+
+
+## Label encoding
+# none
+
+## Cyclical encoding
+data_encoded_onehot$hour_sin <- sin(2 * pi * data_encoded_onehot$HourOfDay / 24)
+data_encoded_onehot$hour_cos <- cos(2 * pi * data_encoded_onehot$HourOfDay / 24)
+data_encoded_onehot$day_sin <- sin(2 * pi * data_encoded_onehot$DayOfWeek / 7)
+data_encoded_onehot$day_cos <- cos(2 * pi * data_encoded_onehot$DayOfWeek / 7)
+data_encoded <- data_encoded_onehot
+head(data_encoded)
+
+#######################
+# Feature Engineering #
+#######################
+
+# # Interaction terms
+# data$interaction_term <- with(data, variable1 * variable2)
+# 
+# # Polynomial features
+# data$polynomial_term <- with(data, variable1^2)
+
+#########################
+# Data Integrity Checks #
+#########################
+
+# # Remove duplicates
+# data <- data %>% distinct()
+# 
+# # Validity checks
+# data <- data %>%
+#   filter(age >= 0)  # Example: Ensuring age is non-negative
+
+#########################
+# Splitting the Dataset #
+#########################
+
+# set.seed(123)  # for reproducibility
+# train_indices <- sample(1:nrow(data), 0.7 * nrow(data))
+# train_data <- data[train_indices, ]
+# test_data <- data[-train_indices, ]
+
+
+
+
+
---
+++ scripts/data-raw-extraction.R
@@ -0,0 +1,75 @@
+library(httr)
+library(jsonlite)
+library(lubridate)
+
+# Helper function to make JSON-RPC requests
+make_rpc_call <- function(url, method, params = list(), rpc_username, rpc_password) {
+  body <- toJSON(list(method = method, params = params, id = "1", jsonrpc = "1.0"), auto_unbox = TRUE)
+  response <- POST(url, body = body, encode = "json", authenticate(rpc_username, rpc_password), content_type("application/json"))
+  if (status_code(response) != 200) {
+    print(paste("HTTP error:", status_code(response), "with method", method))
+    print(content(response, "text"))
+    return(NULL)
+  }
+  data <- content(response, as = "parsed", type = "application/json")
+  if (is.null(data$result)) {
+    print(paste("No result found for", method, ": check the JSON-RPC response for errors"))
+    print(data$error)
+  }
+  return(data$result)
+}
+
+# Main function to extract data
+extract_data <- function() {
+  rpc_username <- "bitcoin_user"  
+  rpc_password <- "MdkjV34UfFN1kA87y43dDU54nvP6Qie4DM"  
+  rpc_host <- "127.0.0.1"
+  rpc_port <- "8332"
+  
+  url <- sprintf("http://%s:%s@%s:%s/", rpc_username, rpc_password, rpc_host, rpc_port)
+  
+  current_block_hash <- make_rpc_call(url, "getbestblockhash", rpc_username = rpc_username, rpc_password = rpc_password)
+  block_data <- make_rpc_call(url, "getblock", list(current_block_hash, 2), rpc_username, rpc_password)
+  
+  blocks <- list()
+  count <- 1
+  
+  while(count <= 10000 && !is.null(block_data) && !is.null(block_data$previousblockhash)) {
+    print(paste("Processing block #", count, ": ", block_data$hash, sep=""))
+    block_time <- as_datetime(block_data$time)
+    block_fees <- sapply(block_data$tx, function(tx) ifelse("fee" %in% names(tx), tx$fee, NA))
+     # in processed data?
+    average_fee <- mean(block_fees, na.rm = TRUE)
+    
+    blocks[[count]] <- list(
+      Date = block_time,
+      AverageFee = average_fee,
+      BlockHash = block_data$hash,
+      BlockNumber = block_data$height,
+      BlockSize = block_data$size,
+      TotalTransactions = length(block_data$tx),
+      TotalFees = sum(block_fees, na.rm = TRUE),
+      Difficulty = block_data$difficulty  
+    )
+    
+    current_block_hash <- block_data$previousblockhash
+    block_data <- make_rpc_call(url, "getblock", list(current_block_hash, 2), rpc_username, rpc_password)
+    
+    count <- count + 1
+  }
+  
+  if (length(blocks) > 0) {
+    df <- do.call(rbind.data.frame, blocks)
+    write.csv(df, "/Users/github/bitcoin-transactions-per-block/data/raw/block_data.csv", row.names = FALSE)
+    print("CSV file saved to /Users/github/bitcoin-transactions-per-block/data/raw/block_data.csv")
+  } else {
+    print("No blocks processed.")
+  }
+}
+
+# Run the extraction
+extract_data()
+
+
+
+
