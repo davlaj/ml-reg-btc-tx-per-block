4aba26dad2294135f726733a0241d83e1b4f359e
davlaj <davidlajeunesse1+github@gmail.com>
2024-05-17 01:15:25 -0300

    Small modif to integrate arguments values in _target.R

--- _targets.R
+++ _targets.R
@@ -2,6 +2,6 @@ source("setup-packages.R")
 source("scripts/data-preprocessing.R")
 list(tar_target(dataFormatted, formatting(raw_data)), 
     tar_target(dataMissingValues, missing_values(dataFormatted)), 
-    tar_target(dataOutliers, outliers(dataMissingValues))
+    tar_target(dataOutliers, outliers(dataMissingValues, outliers_to_remove = "AverageFee"))
     #tar_target(dataSplitted, splitting(dataPreprocessed), seed = 123)
     )
